5809009b9552d7b32fedfa6aba79b5144716375a
Josef Behr <mail@josef-behr.de>
2015-07-14 17:12:17 +0200

    updated theano model

--- rnnlm/rnnlm-theano.py
+++ rnnlm/rnnlm-theano.py
@@ -4,6 +4,9 @@ https://github.com/mesnilgr/is13/blob/master/rnn/elman.py
 http://deeplearning.net/tutorial/rnnslu.html
 https://raw.githubusercontent.com/pascanur/DeepLearningBenchmarks/master/rnn/rnn.py
 and now also: https://github.com/gwtaylor/theano-rnn/blob/master/rnn.py
+
+TODO: * test if this does what it's supposed to (how?)
+      * save trained model (and  possibility to load?)
 '''
 
 import theano
@@ -13,10 +16,9 @@ import time
 import numpy
 from collections import OrderedDict
 
-class RNN(object):
-    def __init__(self, input, words, hidden):
+class rnnlm(object):
+    def __init__(self, words, hidden):
         '''
-        x :: data
         words :: number of words / dimension of input
         hidden :: dimension of hidden layer
         '''
@@ -24,49 +26,50 @@ class RNN(object):
         floatX = theano.config.floatX
 
         # Initialize weights
-        self.w_xh = theano.shared(0.2 * numpy.random.uniform(-1.0, 1.0 \
+        self.w_xh = theano.shared(0.2 * numpy.random.uniform(-1.0, 1.0, \
                                   (words, hidden)).astype(floatX))
-        self.w_hh = theano.shared(0.2 * numpy.random.uniform(-1.0, 1.0 \
+        self.w_hh = theano.shared(0.2 * numpy.random.uniform(-1.0, 1.0, \
                                   (hidden, hidden)).astype(floatX))
-        self.w_hy = theano.shared(0.2 * numpy.random.uniform(-1.0, 1.0 \
+        self.w_hy = theano.shared(0.2 * numpy.random.uniform(-1.0, 1.0, \
                                   (hidden, words)).astype(floatX))
-        self.h0 = theano.shared(numpy.zeros(nh, dtype=floatX))
+        self.h0 = theano.shared(numpy.zeros(hidden, dtype=floatX))
 
         self.params = [ self.w_xh, self.w_hh, self.w_hy ]
         self.names = [ 'w_xh', 'w_hh', 'w_hy' ]
 
+        x = T.imatrix()
         def recurrence(x_t, h_tm1):
             h_t = T.nnet.sigmoid(T.dot(x_t, self.w_xh) \
                                  + T.dot(h_tm1, self.w_hh))
-            y_t = T.nnet.softmax(T.dot(h_t, self.w_hy))
-            return [h_t, y_t]
+            s_t = T.nnet.softmax(T.dot(h_t, self.w_hy))
+            return [h_t, s_t]
 
-        [self.h, self.y_g_x], _ = theano.scan(fn=recurrence, \
-                                              sequences=self.input, \
-                                              outputs_info=[self.h0, None])
+        [h, y], _ = theano.scan(fn=recurrence, \
+                                sequences=x, \
+                                outputs_info=[self.h0, None])
 
-        self.y_out = T.argmax(self.y_g_x, axis=-1)
-
-
-        l_r = T.scalar('l_r', dtype=floatX)
-        cost = -T.mean(T.log(y, TT)
+        lr = T.scalar('l_r', dtype=floatX)
+        cost = -T.mean(T.log(y))
 
         gradients = T.grad(cost, self.params)
         updates = OrderedDict((p, p-lr*g) for p, g in zip(self.params, gradients))
-        print(updates)
 
-        idxs = imatrix()
-        self.train = theano.function(inputs=[idxs, t, lr], \
-                                     outputs=costs, \
-                                     updates=updates) 
+        self.train = theano.function(inputs=[x, lr], \
+                                     outputs=cost, \
+                                     updates=updates, \
+                                     allow_input_downcast=True) 
 
-        # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH!
 
 if __name__ == '__main__':
     words = 6000
+    steps = 100
     a = numpy.zeros(words)
     a[0] = 1
+    b = numpy.zeros((steps,words))
     rnn = rnnlm(words, 25);
-    for t in numpy.arange(100):
+    for t in numpy.arange(steps):
         numpy.random.shuffle(a)
-        rnn.train(a, t, 0.1)
+        b[t] = a
+    theano.printing.Print("Params before training:", rnn.params)
+    rnn.train(b, 0.001)
+    theano.printing.Print("Params after training:", rnn.params)
