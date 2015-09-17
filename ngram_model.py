1a0202c16bfc59d747eb5de226c14161be140b80
Moritz <npsjua@gmail.com>
2015-09-17 09:01:21 +0200

    simplified usage of crawler

--- ngram_model.py
+++ ngram_model.py
@@ -2,19 +2,21 @@ import nltk
 
 
 class NgramModel():
-    def __init__(self, word_list, n, voc_size, default_value=0.0001):
+    def __init__(self, word_list, n, voc_size, delta=0.0001):
         assert 1 < n < len(word_list)
 
+        # ngram frequencies
         bgs = nltk.ngrams(word_list, n)
         self.n_fdist = nltk.FreqDist(bgs)
 
+        # (n-1)-gram frequencies
         bgs = nltk.ngrams(word_list, n-1)
         self.nm_fdist = nltk.FreqDist(bgs)
 
         self.n = n
         self.voc_size = voc_size
-        self.norm = (voc_size**n)*default_value + word_list
-        self.default_value
+        self.norm = (voc_size**n)*delta + word_list
+        self.delta = delta
 
 
     def get_prob(self, words):
@@ -25,10 +27,9 @@ class NgramModel():
 
 
     def _get_prob(self, ngram):
-        #assert len(ngram) == len(self.n)
-        #count = self.fdist[ngram]
-        #return (count+self.default_value)/self.norm
-        return self.n_fdist[ngram]/self.nm_fdist[ngram[:-1]]
+        nar = self.n_fdist[ngram]+self.delta
+        den = self.nm_fdist[ngram[:-1]]+self.delta*self.voc_size
+        return nar/den
 
 
 if __name__ == '__main__':
